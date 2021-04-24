b9e97d73f55a4e770f53379eca5cead7f03dcad1
smilegupta <guptamiley3012@gmail.com>
2021-04-25 00:09:06 +0530

    added md previewer

--- src/Components/Screens/MdViewer/MdViewer.js
+++ src/Components/Screens/MdViewer/MdViewer.js
@@ -1,28 +1,42 @@
-import { Fragment } from 'react'
-import { Row, Col} from "react-bootstrap";
+import { Fragment, useState } from "react";
+import { Row, Col } from "react-bootstrap";
 import { Link } from "react-router-dom";
-// import ReactMarkdown from 'react-markdown'
+import ReactMarkdown from "react-markdown";
+import { markdownText } from "../../../helper";
 
 const MdViewer = () => {
-    return (
-        <Fragment>
-            <Row className="mb-3">
-                <Col className="text-right">
-                    <Link to="/">
-                        <i className="las la-arrow-left" /> Go Back
-            </Link>
-                </Col>
-            </Row>
-            <Row>
-                <Col sm={12} lg={12} xl={12} md={12} className="no-gutters">
-                    <h4>
-                        MD Viewer
-                    </h4>
-                    <br />
-                </Col>
-            </Row>
-        </Fragment>
-    )
-}
+  // State Variables
+  const [markdown, setMarkdown] = useState(markdownText);
+  return (
+    <Fragment>
+      <Row className="mb-3">
+        <Col className="text-right">
+          <Link to="/">
+            <i className="las la-arrow-left" /> Go Back
+          </Link>
+        </Col>
+      </Row>
+      <Row>
+        <Col sm={12} lg={12} xl={12} md={12} className="no-gutters">
+          <h4>MD Viewer</h4>
+          <br />
+        </Col>
+      </Row>
+      <Row>
+        <Col sm={12} lg={6} xl={6} md={6} xs={12}>
+          <textarea
+            style={{ borderRadius: "0px", minHeight: "70vh" }}
+            className="form-control w-100 h-100 p-4"
+            value={markdown}
+            onChange={(e) => setMarkdown(e.target.value)}
+          />
+        </Col>
+        <Col sm={12} lg={6} xl={6} md={6} xs={12}>
+          <ReactMarkdown>{markdown}</ReactMarkdown>
+        </Col>
+      </Row>
+    </Fragment>
+  );
+};
 
-export default MdViewer
+export default MdViewer;
--- src/helper.js
+++ src/helper.js
@@ -62,3 +62,56 @@ export const notebooks = [
         "notebookId": "5ced0001-004b-43d0-b778-df33242ce62e"
     }
 ]
+
+
+export const markdownText = 
+` # Markdown syntax guide
+
+## Headers
+
+# This is a Heading h1
+## This is a Heading h2 
+### This is a Heading h3
+#### This is a Heading h4 
+##### This is a Heading h5
+###### This is a Heading h6
+
+## Emphasis
+
+*This text will be italic*  
+_This will also be italic_
+
+**This text will be bold**  
+__This will also be bold__
+
+_You **can** combine them_
+
+## Lists
+
+### Unordered
+
+* Item 1
+* Item 2
+* Item 3
+
+### Ordered
+
+1. Item 1
+2. Item 2
+3. Item 3
+
+
+## Images
+
+![This is a alt text.](https://image-dock-uploads-be.s3.ap-south-1.amazonaws.com/image.2021-04-24T18%3A05%3A09.755Z)
+
+## Links
+
+You may be using [Markdown Live Preview](http://notes-yard.smilegupta.tech/).
+
+## Blockquotes
+
+> Markdown is a lightweight markup language with plain-text-formatting syntax, created in 2004 by John Gruber with Aaron Swartz.
+>
+>> Markdown is often used to format readme files, for writing messages in online discussion forums, and to create rich text using a plain text editor.
+`
