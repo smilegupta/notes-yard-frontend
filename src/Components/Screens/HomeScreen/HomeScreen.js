575ecff8b0202562ede21f48cd9a74caa4835026
smilegupta <guptamiley3012@gmail.com>
2021-04-24 19:32:30 +0530

    mobile responsiveness fixed

--- src/Components/Screens/HomeScreen/HomeScreen.js
+++ src/Components/Screens/HomeScreen/HomeScreen.js
@@ -14,7 +14,7 @@ const HomeScreen = () => {
         </Col>
       </Row>
       <Row>
-        <Col sm={12} lg={12} xl={12} md={12} className="no-gutters">
+        <Col sm={12} lg={12} xl={12} md={12} className="no-gutters text-md-left text-center">
           <h3>
             My Notebooks <i className="las la-plus"></i>
           </h3>
@@ -24,7 +24,7 @@ const HomeScreen = () => {
       <Row>
         {
           notebooks.map((notebook, idx) => (
-            <Col sm={6} lg={3} xl={3} md={3} xs={6} className="no-gutters" key={idx}>
+            <Col sm={6} lg={3} xl={3} md={4} xs={8} className="no-gutters mx-md-0 mx-auto" key={idx}>
               <div className="card notebook-card shadow mb-4" style={{ backgroundImage: `url(${notebook.patternType})` }}>
                 <div className="notebook-cover">
                   <div className="notebook-nameslip p-2">
@@ -39,7 +39,7 @@ const HomeScreen = () => {
             </Col>
           ))
         }
-        <Col sm={6} lg={3} xl={3} md={3} xs={6}>
+        <Col sm={6} lg={3} xl={3} md={4} xs={8} className="no-gutters mx-md-0 mx-auto">
         <div className="card notebook-card shadow mb-4 notebook-empty d-flex align-items-center justify-content-center">
           <i className="las la-plus-circle" style={{fontSize: "22px"}} />
             Create Notebook
